195dafd3171faf1536f171ed7f71274b98ea6a66
Максим <maksim.popov2@yandex.ru>
2021-11-25 18:53:31 +0300

    Random balls

--- Random-balls/main.py
+++ Random-balls/main.py
@@ -2,16 +2,17 @@ import sys
 
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import uic
+# from PyQt5 import uic
+from ui import Ui_Form
 
 from random import randint
 
 
-class MyWidget(QWidget):
+class MyWidget(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        # self.setupUi(self)
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
+        # uic.loadUi('Ui.ui', self)
         self.pushButton.clicked.connect(self.paint)
         self.do_paint = False
     
@@ -27,8 +28,8 @@ class MyWidget(QWidget):
             qp.end()
 
     def draw_balls(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
         for _ in range(randint(10, 50)):
+            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             qp.drawEllipse(randint(120, 200), randint(30, 200), randint(30, 100), randint(30, 100))
 
 
